4b9c1e8f200d09b28f3c942b12bad12d0aa2df10
intent <1428658308@qq.com>
2023-09-20 11:38:33 +0800

    first commit

---
+++ src/main/java/com/example/nacosgatewaytest/NacosGatewayTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.nacosgatewaytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosGatewayTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosGatewayTestApplication.class, args);
+    }
+
+}
